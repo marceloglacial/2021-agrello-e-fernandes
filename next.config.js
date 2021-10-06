2e52fef4ac315e243605929994e50ff4d00c100d
Marcelo <glacial@gmail.com>
2021-10-06 16:30:54 -0400

    add api

--- next.config.js
+++ next.config.js
@@ -1,7 +1,28 @@
-const path = require('path');
-
 module.exports = {
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
+  // Target must be serverless
+  target: 'serverless',
+  images: {
+    domains: ['res.cloudinary.com', 'localhost'],
+  },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ];
   },
 };
