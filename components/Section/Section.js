55636577efab4782f169db98ec256c9c881febc6
Marcelo <glacial@gmail.com>
2021-10-23 19:48:02 -0400

    fix section

--- components/Section/Section.js
+++ components/Section/Section.js
@@ -1,11 +1,11 @@
 const Section = (props) => {
   const { id, title, children, className, dark, page } = props;
   const styles = {
-    section: `${
-      !page ? `py-32` : `py-8`
-    } xl:py-28 px-6 xl:px-0 xl:py-32 relative bg-white`,
+    section: `relative bg-white`,
     title: `bottom-decoration mb-12 p-0 inline-block font-bold`,
-    container: `container mx-auto ${!page && `text-center xl:text-left`}`,
+    container: `container mx-auto px-6 xl:px-0 ${
+      page ? `pt-8` : `pt-32 pb-8 xl:pb-32 text-center xl:text-left`
+    }`,
     dark: `bg-transparent text-white text-center xl:text-left`,
   };
 
--- components/Services/Services.js
+++ components/Services/Services.js
@@ -23,6 +23,6 @@ const Services = (props) => {
 };
 export default Services;
 const styles = {
-  grid: 'px-6 xl:px-0 grid xl:grid-cols-4 gap-8',
+  grid: 'grid xl:grid-cols-4 gap-8',
   item: 'flex items-center justify-center cursor-pointer p-8 border-2 border-yellow-400 text-black hover:bg-yellow-400 hover:text-white transition-colors text-center',
 };
