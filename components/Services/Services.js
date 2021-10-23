173d0c5da58e71766d51e8adc0a83950184e4443
Marcelo <glacial@gmail.com>
2021-10-23 17:52:43 -0400

    sort services

--- components/Services/Services.js
+++ components/Services/Services.js
@@ -1,15 +1,18 @@
 import Link from 'next/link';
 import Section from 'components/Section/Section';
+import sortArray from 'functions/sortArray';
 
 const Services = (props) => {
   const { active, title, data } = props;
 
   if (!active) return '';
 
+  const services = sortArray(data);
+
   return (
     <Section id='servicos' title={title}>
       <div className={styles.grid}>
-        {data.map((item) => (
+        {services.map((item) => (
           <Link key={item.id} href={`/servicos/${item.slug}`}>
             <a className={styles.item}>{item.title}</a>
           </Link>
@@ -21,5 +24,5 @@ const Services = (props) => {
 export default Services;
 const styles = {
   grid: 'grid xl:grid-cols-4 gap-8',
-  item: 'cursor-pointer p-8 border-2 border-yellow-400 text-black hover:bg-yellow-400 hover:text-white transition-colors text-center',
+  item: 'flex items-center justify-center cursor-pointer p-8 border-2 border-yellow-400 text-black hover:bg-yellow-400 hover:text-white transition-colors text-center',
 };
